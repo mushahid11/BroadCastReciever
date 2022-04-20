0cccac40154a90e327478226d76b67df18e284f5
mahadaleem.ninesol <mahadaleem.ninesol@gmail.com>
2022-04-20 15:23:11 +0500

    4nd Commit

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -1,16 +1,54 @@
 package com.example.broadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.broadcast.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    private var minuteUpdateReceiver: BroadcastReceiver? = null
+    private var counter = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        setContentView(R.layout.activity_main)
-
-        savedInstanceState?.putBoolean("ok",true)
 
     }
+
+
+    fun startMinuteUpdater() {
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(Intent.ACTION_TIME_TICK)
+        minuteUpdateReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                counter++
+                binding.counterText.text = "" + counter
+            }
+        }
+        registerReceiver(minuteUpdateReceiver, intentFilter)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startMinuteUpdater()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(minuteUpdateReceiver)
+    }
+
 }
